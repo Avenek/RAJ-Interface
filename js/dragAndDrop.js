992c200ab93b098d98b83079cd9ce6c3c31e3d3f
Avenek <j.machnik2002@gmail.com>
2023-08-19 23:23:13 +0200

    LocalStorage to dragAndDrop

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -1,5 +1,6 @@
-const moduleContainers = document.querySelectorAll(".single-module-container");
+let singleModulesContainers = document.querySelectorAll(".single-module-container");
 const containerLists = document.querySelectorAll('.container');
+const moduleContainer = document.querySelector(".module-container")
 
 let draggedModule = null;
 let offsetX = 0;
@@ -19,11 +20,6 @@ function handleDragEnd() {
     draggedModule.classList.remove("dragging");
     offsetX = 0;
     offsetY = 0;
-
-    if (draggedModule.shadow) {
-        document.body.removeChild(draggedModule.shadow);
-        draggedModule.shadow = null;
-    }
     saveState()
 }
 
@@ -43,75 +39,29 @@ function handleDrop(event) {
     }
 }
 
-moduleContainers.forEach((moduleContainer) => {
-    moduleContainer.addEventListener("dragstart", handleDragStart);
-    moduleContainer.addEventListener("dragend", handleDragEnd);
-    moduleContainer.addEventListener("dragover", handleDragOver);
-    moduleContainer.addEventListener("drop", handleDrop);
-});
+function createDOMEvenets() {
+    singleModulesContainers.forEach((singleModuleContainer) => {
+        singleModuleContainer.addEventListener("dragstart", handleDragStart);
+        singleModuleContainer.addEventListener("dragend", handleDragEnd);
+        singleModuleContainer.addEventListener("dragover", handleDragOver);
+        singleModuleContainer.addEventListener("drop", handleDrop);
+    });
+}
 
-document.addEventListener("dragover", (event) => {
-    event.preventDefault();
-
-    if (draggedModule && draggedModule.classList.contains("dragging")) {
-        if (!draggedModule.shadow) {
-            const shadow = draggedModule.cloneNode(true);
-            shadow.classList.remove("dragging");
-            shadow.style.position = "fixed";
-            shadow.style.pointerEvents = "none";
-            shadow.style.opacity = "0.7";
-            shadow.style.zIndex = "1000";
-            shadow.style.transition = "none";
-            shadow.style.transform = "translate(-50%, -50%)";
-
-            document.body.appendChild(shadow);
-            draggedModule.shadow = shadow;
-        }
-
-        draggedModule.shadow.style.left = event.clientX + "px";
-        draggedModule.shadow.style.top = event.clientY + "px";
-    }
-});
 
 function saveState() {
     const mainContainerHTML = document.querySelector('.module-container').innerHTML;
-
     localStorage.setItem('srajContainerState', mainContainerHTML);
-}
-
-/*function addDragAndDropListeners() {
-    moduleContainers.forEach((moduleContainer) => {
-        moduleContainer.addEventListener("dragstart", handleDragStart);
-        moduleContainer.addEventListener("dragend", handleDragEnd);
-        moduleContainer.addEventListener("dragover", handleDragOver);
-        moduleContainer.addEventListener("drop", handleDrop);
-    });
-}
-
-function removeDragAndDropListeners() {
-    moduleContainers.forEach((moduleContainer) => {
-        moduleContainer.removeEventListener("dragstart", handleDragStart);
-        moduleContainer.removeEventListener("dragend", handleDragEnd);
-        moduleContainer.removeEventListener("dragover", handleDragOver);
-        moduleContainer.removeEventListener("drop", handleDrop);
-    });
 }
 
 function restoreState() {
     const mainContainerHTML = localStorage.getItem('srajContainerState');
     if (mainContainerHTML) {
-        const moduleContainer = document.querySelector('.module-container');
         moduleContainer.innerHTML = mainContainerHTML;
-
-        // Usuń stare event listenery
-        removeDragAndDropListeners();
-
-        // Dodaj nowe event listenery
-        addDragAndDropListeners();
     }
+    singleModulesContainers = document.querySelectorAll(".single-module-container");
+    createDOMEvenets()
 }
 
-window.addEventListener('load', () => {
-    restoreState();
-    console.log(localStorage.getItem("srajContainerState"));
-});*/
+document.addEventListener("DOMContentLoaded", restoreState());
+
