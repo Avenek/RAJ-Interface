491dcc9a97f075a3ad6b67b1b41e3d68f7d4f9bc
Avenek <j.machnik2002@gmail.com>
2023-11-03 13:41:06 +0100

    Creating object form list based on config

--- src/models/form/objectFormModel.js
+++ src/models/form/objectFormModel.js
@@ -3,12 +3,47 @@ class ObjectFormModel{
         this.jsonData = jsonData
         this.container = container
         this.jsonDataBox = jsonDataBox
-        this.objectFormList = null
-        this.createObjectFormList()
+        this.objectFormList = []
+        this.fetchConfigAndCreateObjectFormList()
     }
 
-    createObjectFormList = () => {
+    fetchConfigAndCreateObjectFormList = () => {
+        const module = this.jsonData.modulePathParams.module
+        fetch(`config/${module}.json`)
+        .then(response => response.json())
+        .then(config => {
+            this.config = config
+            this.createObjectFormList(this.config)
+            //uzupełnić pola
+            //Sprawdzić wymagania - dodać hide
+            //Zwalidować dane
+            //Podświetlić extra-option
+            this.objectFormChanged()
+        })
+        .catch(error => {
+        console.error('Błąd pobierania:', error);
+        })
+    }
 
+    createObjectFormList = (config) => {
+        for (const property of config.properties) {
+            const propertyObject = this.createObjectProperty(property)
+            this.objectFormList.push(propertyObject)
+            if(propertyObject.hasOwnProperty("properties")){
+                this.createObjectFormList(property)
+            }
+        }
+    }
+
+    createObjectProperty = (property) => {
+        const propertyObject = JSON.parse(JSON.stringify(property))
+        propertyObject.value = propertyObject.defaultInput
+        delete propertyObject.defaultInput
+        propertyObject.hide = false
+        propertyObject.isError = false
+        propertyObject.errorMessage = ""
+        
+        return propertyObject
     }
 
     bindObjectFormChanged = (callback) => {
