65a858852c953633a9f0840778ef16de8595818b
Avenek <j.machnik2002@gmail.com>
2023-11-30 14:08:02 +0100

    Change in files paths

--- src/views/home/modulesBoxView.js
+++ src/views/home/modulesBoxView.js
@@ -22,7 +22,7 @@ class ModulesBoxView extends View{
                     <div class="tool-tip-icon">i</div>
                     <p class="tool-tip-info">${singleModule.tipInfo}</p>
                   </div>
-                <div class="glow-on-hover"><img src="assets/pictures/${singleModule.name.charAt(0).toLowerCase() + singleModule.name.slice(1)}.png" alt="${singleModule.name}"><br>${singleModule.name}</div>
+                <div class="glow-on-hover"><img src="src/assets/pictures/${singleModule.name.charAt(0).toLowerCase() + singleModule.name.slice(1)}.png" alt="${singleModule.name}"><br>${singleModule.name}</div>
             </div>\n`
             }
             containerDiv+="</div>"
